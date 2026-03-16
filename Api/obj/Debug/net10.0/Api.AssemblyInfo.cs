0063c3408535d228a95d14144ef63167b01b7dbe
Guilherme Mitsuzumi <mitsuzumiguilhermeryu@gmail.com>
2026-03-16 17:02:51 -0300

    Reapply "feat(backend) Adição da padronização e limpeza de dados/ validação de insights"
    This reverts commit 130d1c5619f8103def880d3387790463ce7e7548.

--- Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ee79e193298e38ec339fa7afaca1928fb2d32b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3098023d9bea2d46ca46455ac2454cebbe805bdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
