b14475b11e1de1294faade044e13c906ac6ad051
Renan Hikaru <167352694+Ctr1A1tDel@users.noreply.github.com>
2026-06-12 16:25:03 -0300

    Revert "Ajustes da Arquitetura DDD"

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,3 @@
-using Api.Application.Dashboard;
-using Api.Domain.Dashboard;
-using Api.Domain.Dashboard.Repositories;
-using Api.Infrastructure.Repositories;
 using OfficeOpenXml;
 
 ExcelPackage.License.SetNonCommercialPersonal("Guilherme");
@@ -27,22 +23,6 @@ builder.Services.AddControllers();
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Configurar CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowReactApp", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
-
-// Registrar serviços DDD
-builder.Services.AddScoped<DashboardDomainService>();
-builder.Services.AddScoped<IDashboardRepository, InMemoryDashboardRepository>();
-builder.Services.AddScoped<DashboardApplicationService>();
 
 var app = builder.Build();
 
